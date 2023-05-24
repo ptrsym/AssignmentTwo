0daf8fa0595d93ec04d86e7f8e61be47881875bc
Peter <ptr.sym@gmail.com>
2023-05-24 11:28:33 +1000

    Establishing CoreData and Persistence files. Creating ReadMe

---
+++ AssignmentTwo/Persistence.swift
@@ -0,0 +1,33 @@
+//
+//  Persistence.swift
+//  AssignmentTwo
+//
+//  Created by Peter on 24/5/2023.
+//
+
+
+import Foundation
+import CoreData
+
+struct PersistenceHandler {
+    static let shared = PersistenceHandler()
+    let container: NSPersistentContainer
+    
+    init (){
+        container = NSPersistentContainer(name: "Model")
+        container.loadPersistentStores {_, error in
+            if let err = error {
+                fatalError("Error to load with \(err)")
+            }
+        }
+    }
+}
+
+func saveData() {
+    let context = PersistenceHandler.shared.container.viewContext
+    do {
+        try context.save()
+    }catch {
+        print("Error saving: \(error)")
+    }
+}
